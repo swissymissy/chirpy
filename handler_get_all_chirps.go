5859af8a964e9734b1ce2b010c608cc5373a904c
swissymissy <pnghi1606@gmail.com>
2026-02-19 16:51:08 -0500

    add sort parameter in url to sort order of chirp list in response

--- handler_get_all_chirps.go
+++ handler_get_all_chirps.go
@@ -3,62 +3,70 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"sort"
 
 	"github.com/google/uuid"
+	"github.com/swissymissy/chirpy/internal/database"
 )
 
 func (apicfg *apiConfig) handlerGetAllChirps(w http.ResponseWriter, r *http.Request) {
 	var responseFmt []resFormat
 	
 	// grab query parameter from URL, there is one
-	authorId := r.URL.Query().Get("author_id")
+	authorId := r.URL.Query().Get("author_id")				// if author_id is given
+	sortPara := r.URL.Query().Get("sort")					// if sort is given
+	desc := false
+	if sortPara == "desc" {
+		desc = true
+	}
+
+	// fetch the data according to the paramater in url
+	var chirpList []database.Chirp
+	var err error
 
 	if authorId == "" {
-		chirpList, err := apicfg.DB.GetAllChirps(r.Context())
+		chirpList, err = apicfg.DB.GetAllChirps(r.Context())					
 		if err != nil {
 			fmt.Printf("Error getting all chirps from db: %s\n", err)
 			msg := "Couldn't get all chirps!"
 			respondWithError(w, 500, msg)
 			return 
 		}
-
-		// writing each item from chirpList to response format
-		for _, eachChirp := range chirpList{
-			responseFmt = append(responseFmt, resFormat{
-					ID: eachChirp.ID,
-					CreatedAt: eachChirp.CreatedAt,
-					UpdatedAt: eachChirp.UpdatedAt,
-					Body: eachChirp.Body,
-					UserID: eachChirp.UserID,
-				})
-		}
-		respondWithJSON(w, 200, responseFmt)
 	} else {
 		// convert ID string to UUID
-		userID, err := uuid.Parse(authorId)
+		var userID uuid.UUID
+		userID, err = uuid.Parse(authorId)
 		if err != nil {
-			respondWithError(w, 404, "Invalid ID")
+			respondWithError(w, 400, "Invalid ID")
 			return
 		}
 		
 		// get list of chirps created by this userID
-		chirpList, err := apicfg.DB.GetAllChirpsFromUserID(r.Context(), userID)
+		chirpList, err = apicfg.DB.GetAllChirpsFromUserID(r.Context(), userID)
 		if err != nil {
 			fmt.Printf("Error getting all chirps from db: %s\n", err)
 			msg := "Couldn't get all chirps!"
 			respondWithError(w, 500, msg)
 			return
 		}
-		// writing each item from chirpList to response format
-		for _, eachChirp := range chirpList{
-			responseFmt = append(responseFmt, resFormat{
-					ID: eachChirp.ID,
-					CreatedAt: eachChirp.CreatedAt,
-					UpdatedAt: eachChirp.UpdatedAt,
-					Body: eachChirp.Body,
-					UserID: eachChirp.UserID,
-				})
-		}
-		respondWithJSON(w, 200, responseFmt)
 	}
+
+	// user wants desc order
+	if desc {
+		sort.Slice(chirpList, func(i, j int) bool {
+			return chirpList[i].CreatedAt.After(chirpList[j].CreatedAt) 
+		})
+	}
+
+	// writing each item from chirpList to response format
+	for _, eachChirp := range chirpList{
+		responseFmt = append(responseFmt, resFormat{
+				ID: eachChirp.ID,
+				CreatedAt: eachChirp.CreatedAt,
+				UpdatedAt: eachChirp.UpdatedAt,
+				Body: eachChirp.Body,
+				UserID: eachChirp.UserID,
+			})
+	}
+	respondWithJSON(w, 200, responseFmt)
 }
